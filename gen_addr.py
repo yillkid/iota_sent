788d100044ca57f61107b534f7889bcc3d5b526e
yillkid <yillkid@gmail.com>
2018-02-09 17:18:09 +0800

    a example for generating address

---
+++ gen_addr.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+from iota import Iota
+from config_iota_wallet import NODE_URL, SEED
+
+api = Iota(NODE_URL, SEED)
+dict_addr = api.get_new_addresses(count = None, index = None)
+
+print "New address: " + str(dict_addr['addresses'])
