d2fcad4c173d7ad07f5834faad244f2a85547b6d
yillkid <yillkid@gmail.com>
2018-11-09 02:22:53 +0800

    Fix find txn

--- find_txn.py
+++ find_txn.py
@@ -3,6 +3,6 @@ from iota import Iota
 from config_iota_wallet import NODE_URL, SEED
 
 api = Iota(NODE_URL, SEED)
-dict_txn = api.find_transactions(tags = ['HAHAHOHOHO'])
+dict_txn = api.find_transactions(tags = ['NTCTODAY'])
 
-print "The tag NTCTODAY transaction hash: " + str(dict_txn['hashes'])
+print("The tag NTCTODAY transaction hash: " + str(dict_txn['hashes']))
