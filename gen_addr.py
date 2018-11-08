76469d48340e5ee3b1f43570361195524725fc1f
yillkid <yillkid@gmail.com>
2018-11-09 02:23:58 +0800

    Fix gen addr

--- gen_addr.py
+++ gen_addr.py
@@ -5,4 +5,4 @@ from config_iota_wallet import NODE_URL, SEED
 api = Iota(NODE_URL, SEED)
 dict_addr = api.get_new_addresses(count = None, index = None)
 
-print "New address: " + str(dict_addr['addresses'])
+print("New address: " + str(dict_addr['addresses']))
